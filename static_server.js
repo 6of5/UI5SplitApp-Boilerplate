41e53ebc7a1fece9e6754c39843bd2540e1c3820
Michael <michael@michaels-mbp.speedport_w_921v_1_37_000>
2015-04-06 13:09:03 +0200

    updated dependencies and fixed some bugs in grunt/server-config

--- static_server.js
+++ static_server.js
@@ -1,22 +1,27 @@
 // Usage see http://scn.sap.com/community/developer-center/front-end/blog/2014/01/05/sapui5-sdk-on-nodejs
+
 var express = require('express'),
+	compression = require('compression'),
+	serveIndex = require('serve-index'),
+	serveStatic = require('serve-static'),
 	open = require('open');
 	app = express(),
 	port = process.env.PORT || 8877,
-	sapui5 = '/UI5SplitApp-Boilerplate'
+	sapui5 = '/ui5-boilerplate',
 	url = 'http://localhost:' + port + sapui5,// + "/latest";
 	year = 60 * 60 * 24 * 365 * 1000;
  
 // Use compress middleware to gzip content
-app.use(express.compress());
+app.use(compression());
  
 //set default mime type to xml for ".library" files
 express.static.mime.default_type = "text/xml";
  
 // Serve up content directory showing hidden (leading dot) files
-app.use(sapui5,express.static(__dirname, { maxAge: year, hidden: true }));
+app.use(sapui5, serveStatic(__dirname, { maxAge: year, dotfiles: 'allow' }));
+
 // enable directory listening
-app.use(sapui5,express.directory(__dirname));
+app.use(sapui5, serveIndex(__dirname));
  
 app.listen(port);
 open(url); //open in default browser
